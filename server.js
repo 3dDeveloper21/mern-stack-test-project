377fafb17f23223d8575f6b0422a07ab85c7828d
Harpreet Singh <3ddeveloper21@gmail.com>
2023-04-20 19:34:29 -0400

    simple middleware set up

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require('express');
 // express app
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
 app.get('/', (req, res) => {
   res.json({msg: 'Welcome to the app'})
 })
